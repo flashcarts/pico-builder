4dc68815dc98aeb3570406756f69d7ad2703b380
Deletecat <deletecat@flashcarts.net>
2026-01-11 17:34:00 +0000

    Package launcher 1.1.0 and loader 1.4.0
    Add CF card packages; pre-dldi patching for certain packages;
    prepend m3powerloader for M3CF

--- packager.py
+++ packager.py
@@ -15,3 +15,14 @@ shutil.copytree("pico_loader", "out", dirs_exist_ok=True)
 if "miniboot_paths" in platforms[current_build]:
     for i in platforms[current_build]["miniboot_paths"]:
         subprocess.run(f"cp -rf miniboot/{i} out", shell=True)
+
+if "dldi" in platforms[current_build]:
+    subprocess.run(["extras/dlditool",f"dldi/{platforms[current_build]["dldi"]}","out/_picoboot.nds"])
+
+if "prepend" in platforms[current_build]:
+    with open("out/_picoboot.ds.gba","wb") as writer, \
+         open(platforms[current_build]["prepend"],"rb") as prepend_file, \
+         open("out/_picoboot.nds","rb") as picoboot:
+
+        writer.write(prepend_file.read())
+        writer.write(picoboot.read())
